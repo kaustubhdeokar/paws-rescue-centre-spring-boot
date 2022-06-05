75568be4972b457fe112afe3ffac865ee2c19cef
kaustubhdeokar <kaustubhd9@gmail.com>
2022-06-05 08:53:59 +0530

    category create, read, update operations added.

--- src/main/java/deokar/kaustubh/pawsrescuecenter/controller/CategoryController.java
+++ src/main/java/deokar/kaustubh/pawsrescuecenter/controller/CategoryController.java
@@ -7,26 +7,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Objects;
 
 @Controller
+@RequestMapping("/category")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
-    @PostMapping("/category/create")
+    @PostMapping("/create")
     public ResponseEntity<ApiResponse> createCategory(@Valid @RequestBody Category category) {
         if (Objects.nonNull(categoryService.readCategory(category.getCategoryName()))) {
             return new ResponseEntity<>(new ApiResponse(false, "already exists"), HttpStatus.CONFLICT);
         }
         categoryService.createCategory(category);
         return new ResponseEntity<>(new ApiResponse(true, "category created"), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> allCategories = categoryService.listCategories();
+        return new ResponseEntity<>(allCategories, HttpStatus.OK);
+    }
+
+    @PostMapping("/update/{categoryID}")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryID") Integer categoryID, @Valid @RequestBody Category category) {
+        if (Objects.nonNull(categoryService.readCategory(categoryID))) {
+            categoryService.updateCategory(categoryID, category);
+            return new ResponseEntity<>(new ApiResponse(true, "category updated"), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new ApiResponse(false, "category does not exist"), HttpStatus.NOT_FOUND);
 
     }
 
--- src/main/java/deokar/kaustubh/pawsrescuecenter/service/CategoryService.java
+++ src/main/java/deokar/kaustubh/pawsrescuecenter/service/CategoryService.java
@@ -5,6 +5,7 @@ import deokar.kaustubh.pawsrescuecenter.repo.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,5 +25,22 @@ public class CategoryService {
 
     public Category readCategory(String categoryName) {
         return categoryRepository.findByCategoryName(categoryName);
+    }
+
+    public List<Category> listCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> readCategory(Integer categoryID) {
+        return categoryRepository.findById(categoryID);
+    }
+
+    public void updateCategory(Integer categoryID, Category newCategory) {
+        Category category = categoryRepository.findById(categoryID).get();
+        category.setCategoryName(newCategory.getCategoryName());
+        category.setDescription(newCategory.getDescription());
+        category.setImageUrl(newCategory.getImageUrl());
+        categoryRepository.save(category);
+
     }
 }
