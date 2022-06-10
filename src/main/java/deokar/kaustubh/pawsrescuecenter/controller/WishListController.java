864df0c505df5bd903e7110a036931a73854c900
kaustubhdeokar <kaustubhd9@gmail.com>
2022-06-10 23:52:23 +0530

    Wishlist get feature added.

--- src/main/java/deokar/kaustubh/pawsrescuecenter/controller/WishListController.java
+++ src/main/java/deokar/kaustubh/pawsrescuecenter/controller/WishListController.java
@@ -8,17 +8,17 @@ import deokar.kaustubh.pawsrescuecenter.model.Product;
 import deokar.kaustubh.pawsrescuecenter.model.User;
 import deokar.kaustubh.pawsrescuecenter.model.WishList;
 import deokar.kaustubh.pawsrescuecenter.repo.ProductRepository;
+import deokar.kaustubh.pawsrescuecenter.repo.UserRepository;
 import deokar.kaustubh.pawsrescuecenter.service.AuthenticationService;
 import deokar.kaustubh.pawsrescuecenter.service.WishListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -50,4 +50,18 @@ public class WishListController {
             throw new CustomException("product not present");
         }
     }
+
+    @GetMapping("/{token}")
+    public ResponseEntity<List<ProductDto>> getWishList(@PathVariable("token") String token) throws AuthenticationFailException {
+
+        authenticationService.authenticate(token);
+        User user = authenticationService.getUser(token);
+        List<WishList> wishLists = wishListService.readWishList(user);
+        List<ProductDto> productDtos = new ArrayList<>();
+        for(WishList wishList:wishLists){
+            productDtos.add(new ProductDto(wishList.getProduct()));
+        }
+        return new ResponseEntity<>(productDtos,HttpStatus.OK);
+    }
+
 }
