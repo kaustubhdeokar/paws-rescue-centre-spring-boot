35e159f52acafb9540e69eb9a6d4b3f1d2bbb8bf
kaustubhdeokar <kaustubhd9@gmail.com>
2022-06-11 23:06:07 +0530

    heroku deployment

--- src/main/java/deokar/kaustubh/pawsrescuecenter/controller/ImageController.java
+++ src/main/java/deokar/kaustubh/pawsrescuecenter/controller/ImageController.java
@@ -1,12 +1,23 @@
 package deokar.kaustubh.pawsrescuecenter.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 @RequestMapping("/images")
 public class ImageController {
 
-
+    @GetMapping(value="/{imagename}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody byte[] getImage(@PathVariable("imagename") String imageName) throws IOException {
+        InputStream resourceAsStream = getClass().getResourceAsStream("/images/"+imageName);
+        return resourceAsStream.readAllBytes();
+    }
 
 }
